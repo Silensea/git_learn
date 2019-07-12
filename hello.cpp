6b2ce23b7651a19cabc9a576cbc9e2b1dcd0b7d8
Silensea <1752064@tongji.edu.cn>
2019-07-12 09:07:20 +0800

    add 2 cpps

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"yej.h"
+int main()
+{
+int a=4,b=5;
+printf("Hello,Linux programming world！\n");
+int result=0;
+result=test(a,b);
+printf("result is",result);
+return 0;
+}
---
+++ hello2.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+printf("Linux programming is FUN!\n");
+return 0;
+}
